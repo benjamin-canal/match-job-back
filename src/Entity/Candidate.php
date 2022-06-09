f2e2cfd9a5d8e3f3bb1457d1138c129620285930
benjamin-canal <benjamin.canal@gmail.com>
2022-06-09 17:56:45 +0200

    entity_candidate_ok

--- src/Entity/Candidate.php
+++ src/Entity/Candidate.php
@@ -67,6 +67,16 @@ class Candidate
      */
     private $portfolio;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updated_at;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -188,6 +198,30 @@ class Candidate
     public function setPortfolio(?string $portfolio): self
     {
         $this->portfolio = $portfolio;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
 
         return $this;
     }
